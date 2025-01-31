0ae6aefae9e285cf6bc3b67138f95e7a63ccad98
acurofobia <acurofobia@gmail.com>
2025-01-31 17:37:59 +0500

    333

--- project_frontend/src/components/Form.jsx
+++ project_frontend/src/components/Form.jsx
@@ -13,9 +13,9 @@ const Form = ({ handleSubmitForm, username, password, setUsername, setPassword, 
     <form className='form' onSubmit={handleSubmit(handleSubmitForm)}>
       <label htmlFor="username">Логин</label>
       <input className='input' type="text" id='username' value={username} 
-        {...register("username", {required: true, pattern: /^([A-Za-z0-9]){1,10}$/gm})} 
+        {...register("username", {required: true, pattern: /^([A-Za-z0-9А-Яа-я]){1,20}$/gm})} 
         onChange={(e) => setUsername(e.target.value)} />
-      <label htmlFor="username">{errors.username && "Логин 1-10 символов, латинские буквы и цифры"}</label>
+      <label htmlFor="username">{errors.username && "Логин 1-10 символов, латинница/кириллица и цифры"}</label>
       <label htmlFor="password">Пароль</label>
       <input className='input' type="password" id='password' value={password} 
         {...register("password", {required: true, pattern: /^([A-Za-z0-9]){1,10}$/gm})}
